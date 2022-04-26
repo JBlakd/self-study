5dc65ab771c733898daccad28b7634487b7c7de8
Ivan Hu <ivanhu2246@gmail.com>
2022-04-26 14:33:55 +0800

    Passed 26/04/2022

--- binary-search/binary_search.cpp
+++ binary-search/binary_search.cpp
@@ -3,58 +3,40 @@
 
 using namespace std;
 
-void print_vector(vector<int> &nums) {
-  printf("[");
-  for (int i = 0; i < nums.size() - 1; i++) {
-    printf("%d, ", nums[i]);
-  }
-  printf("%d]\n", nums[nums.size() - 1]);
-}
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+        }
 
-int search(vector<int> &nums, int target) {
-  print_vector(nums);
-
-  if (nums.size() == 1) {
-    if (target == nums[0]) {
-      return 0;
-    } else {
-      return -1;
-    }
-  }
-
-  auto left = nums.begin();
-  auto right = nums.end();
-  --right;
-  auto mid = left + (distance(left, right) / 2);
-
-  while (*left <= *right) {
-    if (target < *mid) {
-      if (mid == nums.begin()) {
         return -1;
-      }
-      right = mid - 1;
-    } else if (target > *mid) {
-      if (mid == (nums.end() - 1)) {
-        return -1;
-      }
-      left = mid + 1;
-    } else {
-      return distance(nums.begin(), mid);
     }
-    mid = left + (distance(left, right) / 2);
-  }
+};
 
-  printf("left: %d, right: %d, mid: %d\n", *left, *right, *mid);
-  return -1;
-}
+int main() {
+    Solution solution;
+    vector<int> input;
+    int target;
 
-int main(void) {
-  // vector<int> nums = {-1, 0, 3, 5, 9, 12};
-  vector<int> nums = {2, 5};
+    input = {5};
+    target = 5;
+    cout << solution.search(input, target) << endl;
 
-  int result = search(nums, 0);
+    input = {-1, 0, 3, 5, 9, 12};
+    target = 2;
+    cout << solution.search(input, target) << endl;
 
-  printf("%d\n", result);
-
-  return 0;
+    input = {-1, 0, 3, 5, 9, 12};
+    target = 9;
+    cout << solution.search(input, target) << endl;
 }
