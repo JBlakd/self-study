bde6307f565630ba0ac42e6be1762c4bb001653e
Ivan Hu <ivanhu2246@gmail.com>
2022-04-28 22:54:01 +0800

    TODO
    https://leetcode.com/problems/reconstruct-itinerary/discuss/78799/Very-Straightforward-DFS-Solution-with-Detailed-Explanations

--- graph-reconstruct-itinerary/find_itinerary.cpp
+++ graph-reconstruct-itinerary/find_itinerary.cpp
@@ -8,9 +8,8 @@ using namespace std;
 class Solution {
    public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        unordered_map<string, priority_queue<string, vector<string>, greater<string>>> adj_list;
-
         // Parse ticket vector into adj_list
+        unordered_map<string, priority_queue<string, vector<string>, greater<string>>> adj_list;
         for (auto ticket : tickets) {
             string& src = ticket[0];
             string& dest = ticket[1];
@@ -27,30 +26,9 @@ class Solution {
             }
         }
 
-        // TODO have to use a queue otherwise could end up in a location with no ticket out,
-        // while still having other tickets in your wallet
-        // Actually no need to have a queue as long as you ensure the priority queue in the adjacency list is working properly
-        // Because the question always guarantees working input
-        // Well actually working input doesn't guarantee that you won't be stranded, so you need a queue after all
-        // i.e. testcase  {{"JFK, KUL"}, {"JFK", "NRT"}, {"NRT", "JFK"}}
-
         vector<string> itinerary;
-        int tickets_left = tickets.size();
-        string cur_location = "JFK";
-        itinerary.push_back("JFK");
 
-        while (tickets_left > 0) {
-            // Fly to a connection with highest alphabetical order from cur_location
-            string next_location = adj_list.at(cur_location).top();
-            adj_list.at(cur_location).pop();
-
-            // They take your ticket
-            --tickets_left;
-
-            // You've arrived!
-            cur_location = next_location;
-            itinerary.push_back(cur_location);
-        }
+        // TODO https://leetcode.com/problems/reconstruct-itinerary/discuss/78799/Very-Straightforward-DFS-Solution-with-Detailed-Explanations
 
         return itinerary;
     }
