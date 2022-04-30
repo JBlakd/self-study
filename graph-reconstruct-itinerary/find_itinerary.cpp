41d35bff187b25e0e96d6fc79b318a103e334ddf
Ivan Hu <ivanhu2246@gmail.com>
2022-04-30 15:28:13 +0800

    Works but THIS IS NOT MY SOLUTION. Only using as reference

--- graph-reconstruct-itinerary/find_itinerary.cpp
+++ graph-reconstruct-itinerary/find_itinerary.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <set>
 #include <unordered_map>
 #include <vector>
 
@@ -8,57 +9,23 @@ using namespace std;
 class Solution {
    public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        adj_list.clear();
+        unordered_map<string, multiset<string>> targets;
+        vector<string> route;
 
-        // Parse ticket vector into adj_list
-        for (auto ticket : tickets) {
-            string& src = ticket[0];
-            string& dest = ticket[1];
-
-            // If not in adj_list
-            if (adj_list.find(src) == adj_list.end()) {
-                // Use stack memory to optimise for speed
-                priority_queue<string, vector<string>, greater<string>> temp_pq;
-                temp_pq.push(dest);
-                // New key entry in adj_list
-                adj_list.insert(make_pair(src, temp_pq));
-            } else {
-                adj_list.at(src).push(dest);
-            }
-        }
-
-        vector<string> itinerary;
-        itinerary.push_back(string("JFK"));
-        int tickets_remaining = tickets.size();
-        dfs("JFK", tickets_remaining, itinerary);
-        return itinerary;
+        for (auto ticket : tickets)
+            targets[ticket[0]].insert(ticket[1]);
+        visit("JFK", targets, route);
+        return vector<string>(route.rbegin(), route.rend());
     }
 
    private:
-    unordered_map<string, priority_queue<string, vector<string>, greater<string>>> adj_list;
-    
-    void dfs(string cur_location, int& tickets_remaining, vector<string>& itinerary) {
-        if (tickets_remaining == 0) {
-            return;
+    void visit(string airport, unordered_map<string, multiset<string>>& targets, vector<string>& route) {
+        while (targets[airport].size()) {
+            string next = *targets[airport].begin();
+            targets[airport].erase(targets[airport].begin());
+            visit(next, targets, route);
         }
-        // Copy the adj list into the current scope
-        auto temp_adj = adj_list.at(cur_location);
-        while (!temp_adj.empty()) {
-            string next_location = temp_adj.top();
-            temp_adj.pop();
-            itinerary.push_back(next_location);
-            --tickets_remaining;
-            dfs(next_location, tickets_remaining, itinerary);
-            if (tickets_remaining == 0) {
-                // found a solution
-                return;
-            }
-            // If we're here that means we're stuck
-            // refund ticket, revert itinerary
-            temp_adj.push(next_location);
-            ++tickets_remaining;
-            itinerary.pop_back();
-        }
+        route.push_back(airport);
     }
 };
 
