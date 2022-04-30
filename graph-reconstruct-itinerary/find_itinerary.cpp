0aa35ccff59fabf98b03cd3f0d96b6029011631b
Ivan Hu <ivanhu2246@gmail.com>
2022-04-30 15:38:19 +0800

    Added plain English plan to implement Hierholzer's Algorithm

--- graph-reconstruct-itinerary/find_itinerary.cpp
+++ graph-reconstruct-itinerary/find_itinerary.cpp
@@ -9,23 +9,24 @@ using namespace std;
 class Solution {
    public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        unordered_map<string, multiset<string>> targets;
-        vector<string> route;
+        /**
+         *
+         *  Hierholzer's Algorithm
+         *  https://www.youtube.com/watch?v=8MpoO2zA2l4
+         *
+         **/
 
-        for (auto ticket : tickets)
-            targets[ticket[0]].insert(ticket[1]);
-        visit("JFK", targets, route);
-        return vector<string>(route.rbegin(), route.rend());
+        // DFS until we get stuck, spending tickets with every DFS
+        // Return until we reach a node that still has outbound tickets.
+        // As we're returning, push_front the current node to the itinerary
+        // That's it brah
+
+        // TODO
+        return nullptr;
     }
 
    private:
-    void visit(string airport, unordered_map<string, multiset<string>>& targets, vector<string>& route) {
-        while (targets[airport].size()) {
-            string next = *targets[airport].begin();
-            targets[airport].erase(targets[airport].begin());
-            visit(next, targets, route);
-        }
-        route.push_back(airport);
+    void visit(string airport) {
     }
 };
 
