f915be9dd8a5ba9c54067b2a187b155c87d829c5
Ivan Hu <ivanhu2246@gmail.com>
2022-04-30 15:58:31 +0800

    Pass 30/04/2022 WE DID IT BOYS SHOUTOUT TO HIERHOLZER WHO DID THIS LEETCODE HARD IN 1873

--- graph-reconstruct-itinerary/find_itinerary.cpp
+++ graph-reconstruct-itinerary/find_itinerary.cpp
@@ -17,16 +17,44 @@ class Solution {
          **/
 
         // DFS until we get stuck, spending tickets with every DFS
-        // Return until we reach a node that still has outbound tickets.
+        // When we get stuck, return until we reach a node that still has outbound tickets. This is the magical ticket-free backtracking process
         // As we're returning, push_front the current node to the itinerary
         // That's it brah
 
-        // TODO
-        return nullptr;
+        // Parse ticket vector into adj_list
+        unordered_map<string, priority_queue<string, vector<string>, greater<string>>> adj_list;
+        for (auto ticket : tickets) {
+            string& src = ticket[0];
+            string& dest = ticket[1];
+
+            // If not in adj_list
+            if (adj_list.find(src) == adj_list.end()) {
+                // Use stack memory to optimise for speed
+                priority_queue<string, vector<string>, greater<string>> temp_pq;
+                temp_pq.push(dest);
+                // New key entry in adj_list
+                adj_list.insert(make_pair(src, temp_pq));
+            } else {
+                adj_list.at(src).push(dest);
+            }
+        }
+
+        vector<string> itinerary;
+        dfs("JFK", itinerary, adj_list);
+        return vector<string>(itinerary.rbegin(), itinerary.rend());
     }
 
    private:
-    void visit(string airport) {
+    void dfs(string cur_loc, vector<string>& itinerary, unordered_map<string, priority_queue<string, vector<string>, greater<string>>>& adj_list) {
+        // Keep doing the DFS
+        while (adj_list.find(cur_loc) != adj_list.end() && !adj_list.at(cur_loc).empty()) {
+            // And spend those tickets!
+            string next_loc = adj_list.at(cur_loc).top();
+            adj_list.at(cur_loc).pop();
+            dfs(next_loc, itinerary, adj_list);
+        }
+        // uh-oh we're stuck with no ticket back, time to magically backtrack
+        itinerary.push_back(cur_loc);
     }
 };
 
