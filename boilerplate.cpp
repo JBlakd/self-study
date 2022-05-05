560940e46c05b56360a802456dafec4d0472cd3f
Ivan Hu <ivanhu2246@gmail.com>
2022-05-05 17:15:14 +0800

    Add print vector function

--- boilerplate.cpp
+++ boilerplate.cpp
@@ -6,6 +6,18 @@ using namespace std;
 class Solution {
 };
 
+template <typename T>
+void print_vector(vector<T> vec) {
+    cout << "{";
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i];
+        if (i != vec.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "}";
+}
+
 int main() {
     Solution solution;
 }
