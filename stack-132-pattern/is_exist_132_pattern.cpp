efc16e41ba8e6ef895061bd2bc07ddd5fcaed58f
Ivan Hu <ivanhu2246@gmail.com>
2022-05-09 14:48:25 +0800

    INTUITIVE UNDERSTANDING STILL ELUDES ME
    I spent 3.5 hours staring at various solutions. Video solutions as well as written solutions. I simply could not get it. Am I dumb or something?

--- stack-132-pattern/is_exist_132_pattern.cpp
+++ stack-132-pattern/is_exist_132_pattern.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 #include <string>
 #include <vector>
 
@@ -7,36 +8,21 @@ using namespace std;
 class Solution {
    public:
     bool find132pattern(vector<int>& nums) {
-        if (nums.size() < 3) {
-            return false;
-        }
+        stack<int> st;
+        int third = -1000000001;
 
-        // As soon as increase is detected, keep track of
-        // - beginning of increase
-        // - current maximum
-        // - first value which violates monotonic increase. Check this value for 132 condition
-
-        bool increasing = nums[1] > nums[0];
-        int local_min = nums[0];
-        int local_max = nums[1];
-        for (int i = 2; i < nums.size(); ++i) {
-            if (increasing) {
-                if (nums[i] > local_min && nums[i] < local_max) {
-                    return true;
-                } else if (nums[i] < local_min && nums[i] < local_max) {
-                    increasing = false;
-                    local_min = nums[i];
-                }
-                // else increasing remains true (untouched)
-            } else {
-                // if not already increasing, the we can't return anything. 
-                // Most we can do is to change the increasing boolean to true, and to change the local_min or max variables
-                if (nums[i] > nums[i - 1]) {
-                    increasing = true;
-                    local_min = nums[i - 1];
-                    local_max = nums[i];
-                }
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            // third is always the second-biggest element encountered so far
+            // so if we encounter a value smaller than this, we can return true
+            if (nums[i] < third) {
+                return true;
             }
+            // If we encounter a value bigger than the top of the stack, pop until we encounter a value smaller or equal
+            while (!st.empty() && nums[i] > st.top()) {
+                third = st.top();
+                st.pop();
+            }
+            st.push(nums[i]);
         }
 
         return false;
@@ -48,15 +34,19 @@ int main() {
     vector<int> input;
 
     // true
+    input = {2, 3, 4, 5, 1, 2, 3};
+    cout << (solution.find132pattern(input) ? "true" : "false") << endl;
+
+    // true
+    input = {3, 5, 4, 3, 4};
+    cout << (solution.find132pattern(input) ? "true" : "false") << endl;
+
+    // true
     input = {3, 1, 4, 2};
     cout << (solution.find132pattern(input) ? "true" : "false") << endl;
 
     // false (this testcase defeats monotonic increase/decrease approach)
     input = {3, 6, 3};
-    cout << (solution.find132pattern(input) ? "true" : "false") << endl;
-
-    // true
-    input = {3, 5, 0, 3, 4};
     cout << (solution.find132pattern(input) ? "true" : "false") << endl;
 
     // false
