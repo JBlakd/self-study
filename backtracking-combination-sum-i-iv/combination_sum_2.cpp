dbf3e6e336d2f88452fad82678144c844062576d
Ivan Hu <ivanhu2246@gmail.com>
2022-05-10 21:14:30 +0800

    Pass 10/05/2022

--- backtracking-combination-sum-i-iv/combination_sum_2.cpp
+++ backtracking-combination-sum-i-iv/combination_sum_2.cpp
@@ -1,56 +1,58 @@
 #include <algorithm>
 #include <iostream>
 #include <string>
-#include <unordered_set>
 #include <vector>
 
 using namespace std;
 
 class Solution {
    public:
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        // Sort the array. Key difference compared to Comb Sum I
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
         vector<int> cur_vec = {};
         int cur_sum = 0;
-        // FAIL
-        unordered_set<vector<int>> ret_set;
-
+        vector<vector<int>> ret = {};
         // Start at all indices
-        for (int cur_index = 0; cur_index < candidates.size(); ++cur_index) {
-            // Avoid duplicates. Key difference compared to Comb Sum I
-            if (cur_index >= 1 && candidates[cur_index] == candidates[cur_index - 1]) {
-                continue;
-            }
-            dfs(cur_vec, cur_sum, ret_set, candidates, cur_index, target);
-        }
-
-        vector<vector<int>> ret(ret_set.begin(), ret_set.end());
+        dfs(cur_vec, cur_sum, ret, candidates, 0, target);
         return ret;
     }
 
    private:
-    void dfs(vector<int>& cur_vec, int& cur_sum, unordered_set<vector<int>>& ret_set, vector<int>& candidates, int cur_index, int target) {
-        cur_vec.push_back(candidates[cur_index]);
-        cur_sum += candidates[cur_index];
-
-        // cout << "Exploring ";
+    void dfs(vector<int>& cur_vec, int& cur_sum, vector<vector<int>>& ret, vector<int>& candidates, int cur_index, int target) {
+        // cout << "Explored: ";
         // print_vector(cur_vec);
-        // cout << " Sum: " << cur_sum << " ";
 
-        if (cur_sum >= target) {
-            if (cur_sum == target) {
-                // cout << " (worked)";
-                if (ret_set.find(cur_vec) == ret_set.end()) {
-                    ret_set.insert(cur_vec);
-                }
-            }
-
-            // backtrack
-            cur_sum -= candidates[cur_index];
-            cur_vec.pop_back();
+        // found!
+        if (cur_sum == target) {
+            ret.push_back(cur_vec);
+            // cout << " found!";
             // cout << endl;
             return;
+        }
+
+        // don't continue if sum exceeds target
+        if (cur_sum > target) {
+            // cout << endl;
+            return;
+        }
+
+        // cout << endl;
+
+        // At this point, can continue to explore all indices
+        while (cur_index < candidates.size()) {
+            cur_vec.push_back(candidates[cur_index]);
+            cur_sum += candidates[cur_index];
+            // Don't explore the same element for the next recursion, explore the next one instead
+            dfs(cur_vec, cur_sum, ret, candidates, cur_index + 1, target);
+
+            // after return, backtrack
+            cur_sum -= candidates[cur_index];
+            cur_vec.pop_back();
+            int just_explored = candidates[cur_index];
+            // during backtracking, only explore the next index containing a different element
+            while (cur_index < candidates.size() && candidates[cur_index] == just_explored) {
+                ++cur_index;
+            }
         }
     }
 
@@ -85,9 +87,30 @@ int main() {
     int target;
     vector<vector<int>> output;
 
-    candidates = {2, 5, 2, 1, 2, 2};
-    target = 7;
-    output = solution.combinationSum(candidates, target);
+    // {}
+    candidates = {2};
+    target = 1;
+    output = solution.combinationSum2(candidates, target);
+    for (auto vec : output) {
+        print_vector(vec);
+        cout << ", ";
+    }
+    cout << endl;
+
+    // {1,1,6}, {1,2,5}, {1,7}, {2,6}
+    candidates = {10, 1, 2, 7, 6, 1, 5};
+    target = 8;
+    output = solution.combinationSum2(candidates, target);
+    for (auto vec : output) {
+        print_vector(vec);
+        cout << ", ";
+    }
+    cout << endl;
+
+    // {1,2,2}, {5}
+    candidates = {2, 5, 2, 1, 2};
+    target = 5;
+    output = solution.combinationSum2(candidates, target);
     for (auto vec : output) {
         print_vector(vec);
         cout << ", ";
