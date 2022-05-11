ad8446527fff6b11f8049424f0a89126d8271684
Ivan Hu <ivanhu2246@gmail.com>
2022-05-11 15:37:08 +0800

    Pass 14/05/2022 intuitive understanding after studying a bit of combinatorics

--- backtracking-sorted-vowel-strings/count_vowel_strings.cpp
+++ backtracking-sorted-vowel-strings/count_vowel_strings.cpp
@@ -6,19 +6,17 @@ using namespace std;
 
 class Solution {
    public:
-    int countVowelStrings(int n) {
+    int countVowelStrings(int k) {
         // Intuitive one-liner permutation solution
+        // {a,e,i,o,u}
 
-        // We are simply working with a base 5 counting system.
-        // Think hexadecimal, but base 5 instead of 16. This decimal, but base 5 instead of 10.
-        // In this counting system, how many numbers contain n digits?
-
-        // Let's try to answer that question for the base 10 counting system:
-        // How many 3 digit numbers in our base 10 counting system?
-        // The smallest 3 digit base 10 number is 10^2 = 10^(n-1) = 100
-        // The largest 3 digit base 10 number is 10^3 - 1 = 1000 - 1 = 999
-        // The inclusive size of this range is 999 - 100 + 1
-        // The formula is base^(n-1) - ( base^n - 1 ) + 1 = base^(n-1) - base^n + 2
+        // How many ways to pick n items with replacement from a set of 5?
+        // Use the unordered with replacement formula:
+        // (n+k-1) Choose k
+        // = (n+k-1)! / ( k! (n+k-1-k)! )
+        // = (k + 4)! / ( k! (4)!)
+        // = (k + 4) * (k + 3) * (k + 2) * (k + 1) / 4!
+        return ((k + 4) * (k + 3) * (k + 2) * (k + 1)) / 24;
     }
 };
 
