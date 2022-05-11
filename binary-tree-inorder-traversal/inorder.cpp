eb2621e3fddd2b8051836b06ef4725f61130d7bb
Ivan Hu <ivanhu2246@gmail.com>
2022-05-11 23:08:09 +0800

    Pass 11/05/2022 #basicshit #frommemory #worksfirsttime

---
+++ binary-tree-inorder-traversal/inorder.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> ret;
+        dfs(root, ret);
+        return ret;
+    }
+
+   private:
+    void dfs(TreeNode *cur_node, vector<int> &ret) {
+        if (cur_node == nullptr) {
+            return;
+        }
+
+        dfs(cur_node->left, ret);
+        ret.push_back(cur_node->val);
+        dfs(cur_node->right, ret);
+    }
+};
+
+template <typename T>
+void print_vector(vector<T> vec) {
+    cout << "{";
+    for (int i = 0; i < vec.size(); i++) {
+        cout << vec[i];
+        if (i != vec.size() - 1) {
+            cout << ", ";
+        }
+    }
+    cout << "}";
+}
+
+int main() {
+    Solution solution;
+}
