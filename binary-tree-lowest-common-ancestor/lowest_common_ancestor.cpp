2b132f9963d1cf148cfe536d790baf9bf9e91dc9
Ivan Hu <ivanhu2246@gmail.com>
2022-05-13 17:13:56 +0800

    Pass 13/05/2022 added comments until intuitive understanding is achieved

--- binary-tree-lowest-common-ancestor/lowest_common_ancestor.cpp
+++ binary-tree-lowest-common-ancestor/lowest_common_ancestor.cpp
@@ -29,12 +29,17 @@ class Solution {
             right_search_result = lowestCommonAncestor(root->right, p, q);
         }
 
+        // At this point, the entire subtree at the current root node has been traversed, because postorder traversal, baby!
         if (left_search_result != nullptr && right_search_result != nullptr) {
             // Answer found if both left and right search results have been found
+            // If we reach this case at a subtree root, the full recursion will still only finish once we have have returned from the root
+            // When we finally recurse back to the root, it will return either left_search_result or right_search_result, corresponding to the side of the root in which the answer lies
             return root;
         } else if (left_search_result != nullptr) {
+            // If only the left result has been found, float the left result upward
             return left_search_result;
         } else if (right_search_result != nullptr) {
+            // If only the right result has been found, float the right result upward
             return right_search_result;
         }
         // no results found
@@ -60,6 +65,42 @@ int main() {
     vector<int> node_vals;
     TreeNode* p;
     TreeNode* q;
+
+    // p = 4, q = 5, ans = 3
+    node_vals = {1, 2, 3, -1, -1, 4, 5};
+    // Node initialisation
+    for (int val : node_vals) {
+        if (val == -1) {
+            input.push_back(nullptr);
+            continue;
+        }
+        TreeNode* cur_node = new TreeNode(val);
+        input.push_back(cur_node);
+    }
+    // Node connection
+    for (int i = 0; i < input.size(); ++i) {
+        int left_index = (i + 1) * 2 - 1;
+        int right_index = (i + 1) * 2;
+
+        if (input[i] == nullptr) {
+            continue;
+        }
+
+        if (left_index < input.size()) {
+            input[i]->left = input[left_index];
+        }
+        if (right_index < input.size()) {
+            input[i]->right = input[right_index];
+        }
+    }
+    p = input[5];
+    q = input[6];
+    cout << solution.lowestCommonAncestor(input[0], p, q)->val << endl;
+    for (auto node : input) {
+        delete node;
+    }
+    input.clear();
+    node_vals.clear();
 
     // p = 5, q = 6, ans = 4
     node_vals = {3, 4, 9, 5, 6, -1, -1, 7, 8, 10, 11};
