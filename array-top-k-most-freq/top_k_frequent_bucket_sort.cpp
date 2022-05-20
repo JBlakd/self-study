ab4069c98a5d07b5e3923507d2b96116aca15c83
Ivan Hu <ivanhu2246@gmail.com>
2022-05-20 16:26:14 +0800

    Pass 20/05/2022 bucket sort simplified

--- array-top-k-most-freq/top_k_frequent_bucket_sort.cpp
+++ array-top-k-most-freq/top_k_frequent_bucket_sort.cpp
@@ -9,22 +9,17 @@ using namespace std;
 class Solution {
    public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        vector<unordered_set<int>> frequencies(nums.size() + 1);
+        vector<vector<int>> frequencies(nums.size() + 1);
         unordered_map<int, int> each_number_freq;
 
         for (int num : nums) {
             // Update frequency map
-            if (each_number_freq.find(num) == each_number_freq.end()) {
-                each_number_freq.emplace(num, 1);
-            } else {
-                ++each_number_freq[num];
-            }
+            ++each_number_freq[num];
+        }
 
-            // Update vector of sets
-            frequencies[each_number_freq[num]].insert(num);
-            if (frequencies[each_number_freq[num] - 1].find(num) != frequencies[each_number_freq[num] - 1].end()) {
-                frequencies[each_number_freq[num] - 1].erase(num);
-            }
+        // populate bucket
+        for (auto frequency : each_number_freq) {
+            frequencies[frequency.second].push_back(frequency.first);
         }
 
         vector<int> ret;
@@ -32,11 +27,9 @@ class Solution {
             if (frequencies[i].empty()) {
                 continue;
             }
-            while (!frequencies[i].empty()) {
-                int val = *frequencies[i].begin();
-                ret.push_back(val);
-                frequencies[i].erase(val);
-                --k;
+            for (int number : frequencies[i]) {
+                ret.push_back(number);
+                k--;
                 if (k == 0) {
                     return ret;
                 }
