880e6568bc070db3b9bac16661d7f27cf9cf8671
Ivan Hu <ivanhu2246@gmail.com>
2022-06-02 14:54:12 +0800

    Pass 02/06/2022 very tricky

--- dp-01-matrix/binary_matrix.cpp
+++ dp-01-matrix/binary_matrix.cpp
@@ -9,11 +9,14 @@ using namespace std;
 class Solution {
    public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        // Don't use max, too close to overflow
+        vector<vector<int>> ret(mat.size(), vector<int>(mat[0].size(), numeric_limits<int>::max() - 100000));
         // two pass DP
         // Firstly from top left to bottom right, considering top and left neighbours
         for (int i = 0; i < mat.size(); ++i) {
             for (int j = 0; j < mat[0].size(); ++j) {
                 if (mat[i][j] == 0) {
+                    ret[i][j] = 0;
                     continue;
                 }
 
@@ -21,20 +24,21 @@ class Solution {
                 if (i == 0 && j == 0) {
                     continue;
                 } else if (i == 0) {
-                    lowest_neighbour = mat[i][j - 1];
+                    lowest_neighbour = ret[i][j - 1];
                 } else if (j == 0) {
-                    lowest_neighbour = mat[i - 1][j];
+                    lowest_neighbour = ret[i - 1][j];
                 } else {
-                    lowest_neighbour = min(mat[i][j - 1], mat[i - 1][j]);
+                    lowest_neighbour = min(ret[i][j - 1], ret[i - 1][j]);
                 }
 
-                mat[i][j] = lowest_neighbour + 1;
+                ret[i][j] = lowest_neighbour + 1;
             }
         }
         // Secondly from bottom right to top left, considering bottom and right neighbours
         for (int i = mat.size() - 1; i >= 0; --i) {
             for (int j = mat[0].size() - 1; j >= 0; --j) {
                 if (mat[i][j] == 0) {
+                    ret[i][j] = 0;
                     continue;
                 }
 
@@ -42,18 +46,19 @@ class Solution {
                 if (i == mat.size() - 1 && j == mat[0].size() - 1) {
                     continue;
                 } else if (i == mat.size() - 1) {
-                    lowest_neighbour = mat[i][j + 1];
+                    lowest_neighbour = ret[i][j + 1];
                 } else if (j == mat[0].size() - 1) {
-                    lowest_neighbour = mat[i + 1][j];
+                    lowest_neighbour = ret[i + 1][j];
                 } else {
-                    lowest_neighbour = min(mat[i][j + 1], mat[i + 1][j]);
+                    lowest_neighbour = min(ret[i][j + 1], ret[i + 1][j]);
                 }
 
-                mat[i][j] = (i == 0 && j == 0) ? lowest_neighbour + 1 : min(mat[i][j], lowest_neighbour + 1);
+                ret[i][j] = min(ret[i][j], lowest_neighbour + 1);
+                // mat[i][j] = (i == 0 && j == 0) ? lowest_neighbour + 1 : min(mat[i][j], lowest_neighbour + 1);
             }
         }
 
-        return mat;
+        return ret;
     }
 };
 
@@ -72,11 +77,12 @@ void print_vector(vector<T> vec) {
 int main() {
     Solution solution;
     vector<vector<int>> mat;
+    vector<vector<int>> ret;
 
     // [[0,0,0],[0,1,0],[1,2,1]]
     mat = {{1, 1, 0, 0, 1, 0, 0, 1, 1, 0}, {1, 0, 0, 1, 0, 1, 1, 1, 1, 1}, {1, 1, 1, 0, 0, 1, 1, 1, 1, 0}, {0, 1, 1, 1, 0, 1, 1, 1, 1, 1}, {0, 0, 1, 1, 1, 1, 1, 1, 1, 0}, {1, 1, 1, 1, 1, 1, 0, 1, 1, 1}, {0, 1, 1, 1, 1, 1, 1, 0, 0, 1}, {1, 1, 1, 1, 1, 0, 0, 1, 1, 1}, {0, 1, 0, 1, 1, 0, 1, 1, 1, 1}, {1, 1, 1, 0, 1, 0, 1, 1, 1, 1}};
-    solution.updateMatrix(mat);
-    for (auto& row : mat) {
+    ret = solution.updateMatrix(mat);
+    for (auto& row : ret) {
         print_vector(row);
         cout << endl;
     }
@@ -86,8 +92,8 @@ int main() {
     mat = {{0, 0, 0},
            {0, 1, 0},
            {1, 1, 1}};
-    solution.updateMatrix(mat);
-    for (auto& row : mat) {
+    ret = solution.updateMatrix(mat);
+    for (auto& row : ret) {
         print_vector(row);
         cout << endl;
     }
