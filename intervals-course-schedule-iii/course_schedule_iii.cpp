06f6d56ff773ec1012e049ab3fdcfd7732d18ac0
Ivan Hu <ivanhu2246@gmail.com>
2022-06-23 18:02:50 +0800

    Logic still ain't correct

--- intervals-course-schedule-iii/course_schedule_iii.cpp
+++ intervals-course-schedule-iii/course_schedule_iii.cpp
@@ -11,23 +11,14 @@ using namespace std;
 class Solution {
    public:
     int scheduleCourse(vector<vector<int>>& courses) {
-        // preferentially sort the courses by ascending duration, and then by ascending deadline
-        sort(courses.begin(), courses.end(), [](vector<int>& a, vector<int>& b) {
-            // should return true if a is ordered before b
-            if (a[0] == b[0]) {
-                return a[1] < b[1];
-            }
-            return a[0] < b[0];
+        // remove invalid courses (duration longer than the deadline)
+        remove_if(courses.begin(), courses.end(), [](vector<int>& course) {
+            return course[1] < course[0];
         });
-        int current_time_taken = 0;
-        int ret1 = 0;
-        for (vector<int>& course : courses) {
-            // attempt to fit current course into schedule
-            if (current_time_taken + course[0] <= course[1]) {
-                // if this course fits into its own deadline
-                current_time_taken += course[0];
-                ++ret1;
-            }
+
+        // After this check, we should have more than 1 courses to consider
+        if (courses.size() <= 1) {
+            return courses.size();
         }
 
         // preferentially sort the courses by ascending deadline, and then by ascending duration
@@ -38,18 +29,28 @@ class Solution {
             }
             return a[1] < b[1];
         });
-        current_time_taken = 0;
-        int ret2 = 0;
-        for (vector<int>& course : courses) {
-            // attempt to fit current course into schedule
-            if (current_time_taken + course[0] <= course[1]) {
-                // if this course fits into its own deadline
-                current_time_taken += course[0];
-                ++ret2;
+
+        int last_course_start = 0;
+        int total_time_taken = courses[0][0];
+        int ret = 1;
+        for (int i = 1; i < courses.size(); ++i) {
+            // replace the last course with the upcoming course if the upcoming course could do better than the last course
+            // in terms of lessening the total_time_taken
+            if (last_course_start + courses[i][0] < total_time_taken) {
+                // replace
+                total_time_taken = last_course_start + courses[i][0];
+            } else {
+                // reject the next course if it doesn't even fit into its own deadline
+                if (total_time_taken + courses[i][0] > courses[i][1]) {
+                    continue;
+                }
+                last_course_start = total_time_taken;
+                total_time_taken = last_course_start + courses[i][0];
+                ++ret;
             }
         }
 
-        return max(ret1, ret2);
+        return ret;
     }
 };
 
@@ -70,15 +71,15 @@ int main() {
     vector<vector<int>> courses;
     int output;
 
-    courses = {{7, 17}, {3, 12}, {10, 20}, {9, 10}, {5, 20}, {10, 19}, {4, 18}};
-    output = solution.scheduleCourse(courses);
-    cout << output << '\n';
-    assert(output == 4);
-
     courses = {{5, 15}, {3, 19}, {6, 7}, {2, 10}, {5, 16}, {8, 14}, {10, 11}, {2, 19}};
     output = solution.scheduleCourse(courses);
     cout << output << '\n';
     assert(output == 5);
+
+    courses = {{7, 17}, {3, 12}, {10, 20}, {9, 10}, {5, 20}, {10, 19}, {4, 18}};
+    output = solution.scheduleCourse(courses);
+    cout << output << '\n';
+    assert(output == 4);
 
     courses = {{5, 5}, {4, 6}, {2, 6}};
     output = solution.scheduleCourse(courses);
