f4c62a445baa46c66b9b611a725375e955d36e9c
Ivan Hu <ivanhu2246@gmail.com>
2022-06-23 18:35:22 +0800

    Pass 23/06/2022 Learnt an elegant algorithm

--- intervals-course-schedule-iii/course_schedule_iii.cpp
+++ intervals-course-schedule-iii/course_schedule_iii.cpp
@@ -11,46 +11,36 @@ using namespace std;
 class Solution {
    public:
     int scheduleCourse(vector<vector<int>>& courses) {
-        // remove invalid courses (duration longer than the deadline)
-        remove_if(courses.begin(), courses.end(), [](vector<int>& course) {
-            return course[1] < course[0];
-        });
-
-        // After this check, we should have more than 1 courses to consider
-        if (courses.size() <= 1) {
-            return courses.size();
-        }
-
-        // preferentially sort the courses by ascending deadline, and then by ascending duration
+        // preferentially sort the courses by ascending deadline
         sort(courses.begin(), courses.end(), [](vector<int>& a, vector<int>& b) {
             // should return true if a is ordered before b
-            if (a[1] == b[1]) {
-                return a[0] < b[0];
-            }
             return a[1] < b[1];
         });
 
-        int last_course_start = 0;
-        int total_time_taken = courses[0][0];
-        int ret = 1;
-        for (int i = 1; i < courses.size(); ++i) {
-            // replace the last course with the upcoming course if the upcoming course could do better than the last course
-            // in terms of lessening the total_time_taken
-            if (last_course_start + courses[i][0] < total_time_taken) {
-                // replace
-                total_time_taken = last_course_start + courses[i][0];
-            } else {
-                // reject the next course if it doesn't even fit into its own deadline
-                if (total_time_taken + courses[i][0] > courses[i][1]) {
-                    continue;
-                }
-                last_course_start = total_time_taken;
-                total_time_taken = last_course_start + courses[i][0];
-                ++ret;
+        // Iterate through our ascending-deadline courses, pushing each into a priority queue with the longest courses being on top
+        // If the sum of the courses' duration in our priority queue ever exceeds the deadline of the course we're currently considering
+        // pop until the sum fits within the deadline
+        int enqueued_duration = 0;
+        priority_queue<int> durations;
+        for (auto& course : courses) {
+            // just skip impossible courses mate
+            if (course[1] < course[0]) {
+                continue;
+            }
+
+            int& current_deadline = course[1];
+
+            // push into priority queue
+            durations.emplace(course[0]);
+            enqueued_duration += course[0];
+
+            while (enqueued_duration > current_deadline) {
+                enqueued_duration -= durations.top();
+                durations.pop();
             }
         }
 
-        return ret;
+        return durations.size();
     }
 };
 
@@ -71,6 +61,16 @@ int main() {
     vector<vector<int>> courses;
     int output;
 
+    courses = {{100, 2}, {32, 50}};
+    output = solution.scheduleCourse(courses);
+    cout << output << '\n';
+    assert(output == 1);
+
+    courses = {{5, 5}, {4, 6}, {2, 6}};
+    output = solution.scheduleCourse(courses);
+    cout << output << '\n';
+    assert(output == 2);
+
     courses = {{5, 15}, {3, 19}, {6, 7}, {2, 10}, {5, 16}, {8, 14}, {10, 11}, {2, 19}};
     output = solution.scheduleCourse(courses);
     cout << output << '\n';
@@ -80,11 +80,6 @@ int main() {
     output = solution.scheduleCourse(courses);
     cout << output << '\n';
     assert(output == 4);
-
-    courses = {{5, 5}, {4, 6}, {2, 6}};
-    output = solution.scheduleCourse(courses);
-    cout << output << '\n';
-    assert(output == 2);
 
     courses = {{100, 200}, {200, 1300}, {1000, 1250}, {2000, 3200}};
     output = solution.scheduleCourse(courses);
